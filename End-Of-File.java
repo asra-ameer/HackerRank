a12cf0935e1ca2d735ad1cd107c2d4ff8088d696
asra-ameer <151917440+asra-ameer@users.noreply.github.com>
2024-06-05 20:30:56 +0530

    Java End-Of-File

---
+++ End-Of-File.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int i = 1;
+
+         while(sc.hasNext()){
+              String line= sc.nextLine();
+              System.out.print(i+" "+line);
+              i++;
+              System.out.println();
+          }
+
+    }
+}
