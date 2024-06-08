e72dd549ef47e828d345b171aa3425c77a5cd719
asra-ameer <151917440+asra-ameer@users.noreply.github.com>
2024-06-08 19:36:34 +0530

    Java Palindrome

--- String Palindrome.java
+++ String Palindrome.java
@@ -7,22 +7,14 @@ public class Solution {
         
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
-        StringBuilder reverse = new StringBuilder();
-
-        for(int i=0;i<A.length();i++){
-        reverse.insert(0,A.charAt(i));
-        }
-        String reversed=reverse.toString();
-        if(A.equals(reversed)){
+        String reverse = new StringBuilder(A).reverse().toString();
+        if(A.equals(reverse)){
             System.out.println("Yes");
-        }else{
+        }else {
             System.out.println("No");
         }
+        
         sc.close();
-        
-        
-        
-        
     }
 }
 
