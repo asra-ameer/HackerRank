049e2fbeb09ef1df50ae8749c755ad1bbfafedf3
asra-ameer <151917440+asra-ameer@users.noreply.github.com>
2024-06-13 10:49:46 +0530

    Java

--- Method Overriding_superKeyowrd.java
+++ Method Overriding_superKeyowrd.java
@@ -15,7 +15,7 @@ class MotorCycle extends BiCycle{
 	MotorCycle(){
 		System.out.println("Hello I am a motorcycle, I am "+ define_me());
     
-		String temp=super.define_me(); //Fix this line
+		String temp=super.define_me(); 
 
 		System.out.println("My ancestor is a cycle who is "+ temp );
 	}
