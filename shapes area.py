ea4f5c3e08215e781e490c89e476ee62f9400846
asra-ameer <151917440+asra-ameer@users.noreply.github.com>
2024-06-19 15:35:09 +0530

    shapes

--- shapes area.py
+++ shapes area.py
@@ -1,42 +1,17 @@
-!/bin/python
-
-import math
-import os
-import random
-import re
-import sys
-
-
-class Rectangle:
-    def __init__(self,length,width):
-        self.length=length
-        self.width=width
-    def  area(self):
-        return self.length*self.width  
-    pass
-
-class Circle:
-    def __init__(self,radius ):
-        self.radius =radius;
-    def area(self):
-        return self.radius*self.radius*math.pi    
+def average(array):
+    
+    sum=0
+    for a in array:
+        sum=sum+a
+    
+    result =sum/len(array)
+    return float(result)    
         
-    pass
-
+    
+    
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    q = int(raw_input())
-    queries = []
-    for _ in xrange(q):
-        args = raw_input().split()
-        shape_name, params = args[0], map(int, args[1:])
-        if shape_name == "rectangle":
-            a, b = params[0], params[1]
-            shape = Rectangle(a, b)
-        elif shape_name == "circle":
-            r = params[0]
-            shape = Circle(r)
-        else:
-            raise ValueError("invalid shape type")
-        fptr.write("%.2f\n" % shape.area())
-    fptr.close()
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
+   
